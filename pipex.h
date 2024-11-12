fb50584780a5d7672d97f8337947d79168bf28f3
Kim <thkm.grd@gmail.com>
2024-11-12 13:37:29 +0100

    first commit

---
+++ pipex.h
@@ -0,0 +1,21 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   pipex.h                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: kgiraud <kgiraud@student.42lausanne.ch>    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/11/12 13:17:20 by kgiraud           #+#    #+#             */
+/*   Updated: 2024/11/12 13:35:18 by kgiraud          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef PIPEX_H
+# define PIPEX_H
+
+# include <unistd.h>
+# include <fcntl.h>
+# include <stdio.h>
+# include <sys/wait.h>
+
+#endif
